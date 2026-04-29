5536eab96bddbfac2d45ad922cd0d42dfbeda3f8
Haydn Shillington <haydn.shillington@Student.SL.On.Ca>
2026-04-29 06:46:30 -0400

    Added Copper into game (only item for now)

---
+++ src/main/java/net/grim/FirstMod/items/ModItems.java
@@ -0,0 +1,19 @@
+package net.grim.FirstMod.items;
+
+import net.grim.FirstMod.FirstMod;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(FirstMod.MOD_ID);
+
+    public static final DeferredItem<Item> COPPER = ITEMS.register("copper",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
